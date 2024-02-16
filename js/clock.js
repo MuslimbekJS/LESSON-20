f503de395877e6fed0ef609e6b5f271db4766540
“MuslimbekJS” <reactjs077@gmail.com>
2024-02-16 07:56:07 +0500

    qwerty

--- js/clock.js
+++ js/clock.js
@@ -7,16 +7,26 @@ function displayTime() {
     var min = dateTime.getMinutes();
     var sec = dateTime.getSeconds();
     var session = document.getElementById('session');
-    
+
     if (hrs >= 12) {
         session.innerHTML = 'PM';
     } else {
         session.innerHTML = 'AM';
     }
-    
+
     if (hrs > 12) {
         hrs = hrs - 12;
     }
+    if (sec > 10) {
+        // sec.toString(sec.push('0'));
+    }
+    hrs = (hrs < 10) ? '0' + hrs : hrs;
+    min = (min < 10) ? '0' + min : min;
+    // var time = hrs + ':' + min + ':' + sec;
+    sec = (sec < 10) ? '0' + sec : sec;
+    // document.getElementById('time').innerHTML = time;
+    // var timer = setTimeout(displayTime, 1000);
+
 
     document.getElementById('hours').innerHTML = hrs;
     document.getElementById('minutes').innerHTML = min;
